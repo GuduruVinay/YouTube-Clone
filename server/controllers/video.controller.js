72b41c172115c836cf56f7c89b0a9136cee611ef
GuduruVinay <guduruvinay3@gmail.com>
2026-01-09 17:59:11 +0530

    feat(server): Updated Video controller to get, like, dislike a single video

--- server/controllers/video.controller.js
+++ server/controllers/video.controller.js
@@ -1,6 +1,6 @@
 import Video from "../models/Video.model.js";
 
-// Get Random Videos for Home Page
+// GET Random Videos for Home Page
 export async function randomVideos(req, res) {
     try {
         // Fetch 30 random videos
@@ -10,3 +10,45 @@ export async function randomVideos(req, res) {
         return res.status(500).json(err);
     }
 };
+
+// GET Single Video
+export async function getVideo(req, res) {
+    try {
+        const video = await Video.findById(req.params.id);
+        return res.status(200).json(video);
+    } catch(err) {
+        return res.status(500).json(err);
+    }
+}
+
+// PUT Like Video
+export async function likeVideo(req, res) {
+    const id = req.user.id; // From JWT Token
+    const videoId = req.params.videoId;
+
+    try{
+        await Video.findByIdAndUpdate(videoId, {
+            $addToSet: { likes: id }, // Add user ID to likes
+            $pull: { dislikes: id }, // Remove from dislikes if present
+        });
+        return res.status(200).json("The video has been liked.");
+    } catch(err) {
+        return res.status(500).json(err);
+    }
+}
+
+// PUT Dislike Video
+export async function dislikeVideo(req, res) {
+    const id = req.user.id;
+    const videoId = req.params.videoId;
+    
+    try {
+        await Video.findByIdAndUpdate(videoId, {
+            $addToSet: { dislikes: id },
+            $pull: { likes: id }
+        });
+        return res.status(200).json("The video has been disliked.");
+    } catch(err) {
+        return res.status(500).json(err);
+    }
+}
--- server/routes/video.route.js
+++ server/routes/video.route.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { randomVideos } from "../controllers/video.controller.js";
+import { dislikeVideo, getVideo, likeVideo, randomVideos } from "../controllers/video.controller.js";
 
 const router = express.Router();
 
 // Endpoints
 router.get('/random', randomVideos);
+
+router.get('/find/:id', getVideo);
+
+router.put('/like/:videoId', likeVideo);
+
+router.put('/dislike/:videoId', dislikeVideo);
 
 export default router;
